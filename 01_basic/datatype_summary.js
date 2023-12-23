cbf0ff3a1620b397607cc67c88113c3664f34efc
Harshitcoding <135004228+Harshitcoding@users.noreply.github.com>
2023-12-23 07:11:33 +0000

    datatype_summary

---
+++ 01_basic/datatype_summary.js
@@ -0,0 +1,13 @@
+// Q :what is stack over flow ?
+// A :stack over flow store the permitive value 
+
+// premitive values : number , boolen , Bigint , Symbol , String , undefined
+
+//premitive datatypes hamesha stack me save hote h aur isme app jo bhi chiz lenge to uski apko copy hi milta h 
+
+// Q2:what is heap memory
+// A: heap memory store the non permitive vales 
+
+//non-premitive me aap jo bhi chi lenget to vo apko reference dega
+
+// non premivtive values : array , object , function 
