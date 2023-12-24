2086873cbcbdc17f4d585664401a229c41b07310
Harshitcoding <135004228+Harshitcoding@users.noreply.github.com>
2023-12-24 05:28:36 +0000

    objects

---
+++ 02_basic/object.js/objectdistructring.js
@@ -0,0 +1,24 @@
+ //////object distructionn
+
+//   const user_id = {
+//     name: "harshit", 
+//     password: "123456",
+//     country : "india",
+//   }
+
+//   const {password} = user_id
+// console.log(password)
+
+// const course = {
+//     coursename: "js in hindi",
+//     price: "999",
+//     courseInstructor: "hitesh"
+// }
+
+// // course.courseInstructor
+
+// const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor);
+// console.log(instructor);
+console.log("hello world")
