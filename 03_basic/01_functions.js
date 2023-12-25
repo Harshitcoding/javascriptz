c3c7897a59714a2866f72266dc816a2c1e8d2dc6
Harshitcoding <135004228+Harshitcoding@users.noreply.github.com>
2023-12-25 05:02:23 +0000

    function / scope / arrow / truthy

---
+++ 03_basic/01_functions.js
@@ -0,0 +1,47 @@
+// function saymyname () {
+//     console.log("hisenberg");
+// }
+
+// saymyname()
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+// function numbers (number1 , number2 ) {
+//     console.log(number1 + number2)
+// }
+
+// numbers(2,null)
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+// function numbers (number1 , number2 ) {
+//        return (number1 + number2) // return likne ke badd ham kisi bhi variable ko store kar sakte h 
+//      }
+//      const numbersz = numbers(1,2)
+//      console.log(numbersz)
+
+
+// const items = {
+//     product : 'saop',
+//     product2: 'shampoo'
+// }
+                    
+// function cart(items) {
+//     console.log(`product name id ${items.product} and ${items.product2}`)
+// }
+
+// cart(items)
+
+
+//============================================================================
+const items = {
+    product : 'saop',
+    product2: 'shampoo'
+}
+                    
+function cart(items) {
+    console.log(`product name id ${items.product} and ${items.product2}`)
+}
+
+cart({
+    product : " maida",
+    product2 : "sooji"
+})
---
+++ 03_basic/03_Arrow.js
@@ -0,0 +1,52 @@
+// const user = {
+//     NAME : "Harshit",
+//     AGE : 40,
+//     myfunc : function () {
+//         console.log(`mt name is ${this.NAME} and age is ${this.AGE}`)
+//     }
+// }
+// user.myfunc()
+
+user.myfunc()
+// user.username = "sam"
+// user.myfunc()
+
+// console.log(this);
+
+// function chai(){
+//     let username = "hitesh"
+//     console.log(this.username);
+// }
+
+// chai()
+
+// const chai = function () {
+//     let username = "hitesh"
+//     console.log(this.username);
+// }
+
+const chai =  () => {
+    let username = "hitesh"
+    console.log(this);
+}
+
+
+// chai()
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+
+// const addTwo = (num1, num2) =>  num1 + num2
+
+// const addTwo = (num1, num2) => ( num1 + num2 )
+
+const addTwo = (num1, num2) => ({username: "hitesh"})
+
+
+console.log(addTwo(3, 4))
+
+
+// const myArray = [2, 5, 3, 7, 8]
+
+// myArray.forEach()
